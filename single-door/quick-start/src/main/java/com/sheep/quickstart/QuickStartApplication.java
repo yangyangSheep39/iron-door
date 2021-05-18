12f195c7923e0c716813d5c9cdf60aa3ed1ef81d
yangyangSheep <fangyang98@163.com>
2021-05-18 10:36:09 +0800

    去除多余的.gitignore

--- single-door/quick-start/src/main/java/com/sheep/quickstart/QuickStartApplication.java
+++ single-door/quick-start/src/main/java/com/sheep/quickstart/QuickStartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author yangyangSheep
  * @ClassName QuickStartApplication.java
- * @Description Spring Securit单体项目快速开始示例
+ * @Description Spring Security单体项目快速开始示例
  * @createTime 2021/5/9 18:51
  */
 @SpringBootApplication
